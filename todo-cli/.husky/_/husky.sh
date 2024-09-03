f7469454c629eb1d49daf89546a63a7cad0925e4
Sathwik <ssathwik31831@gmail.com>
2024-09-03 11:35:37 +0530

    Test pre-commit hook

--- todo-cli/.husky/_/husky.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-# Husky Git hooks script
-# To use, include this line in your hook:
-# . "$(dirname "$0")/_/husky.sh"
-# For more information, visit https://typicode.github.io/husky/#/
-
-set -e
